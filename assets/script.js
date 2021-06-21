f40b6072c1353f890ebcea09d53c6de7dea03699
Harris Charalambous <h.chara3@gmail.com>
2021-06-21 20:55:01 +1000

    qualitiesText slides in

---
+++ assets/script.js
@@ -0,0 +1,60 @@
+var devText = document.querySelector('#developer');
+var qualitiesText = document.querySelector('#qualitiesText');
+
+function setQualitiesText(quality) {
+  switch (quality) {
+    case 'developer':
+      document.getElementById('qualitiesTextDev').style.display = 'block';
+      document.getElementById('qualitiesTextMusician').style.display = 'none';
+      document.getElementById('qualitiesTextPhotographer').style.display = 'none';
+      document.getElementById('qualitiesTextTester').style.display = 'none';
+      document.getElementById('qualitiesGraphicDesigner').style.display = 'none';
+      document.getElementById('qualitiesTechEnthusiast').style.display = 'none';
+      break;
+
+    case 'musician':
+      document.getElementById('qualitiesTextDev').style.display = 'none';
+      document.getElementById('qualitiesTextMusician').style.display = 'block';
+      document.getElementById('qualitiesTextPhotographer').style.display = 'none';
+      document.getElementById('qualitiesTextTester').style.display = 'none';
+      document.getElementById('qualitiesGraphicDesigner').style.display = 'none';
+      document.getElementById('qualitiesTechEnthusiast').style.display = 'none';
+      break;
+
+    case 'photographer':
+      document.getElementById('qualitiesTextDev').style.display = 'none';
+      document.getElementById('qualitiesTextMusician').style.display = 'none';
+      document.getElementById('qualitiesTextPhotographer').style.display = 'block';
+      document.getElementById('qualitiesTextTester').style.display = 'none';
+      document.getElementById('qualitiesGraphicDesigner').style.display = 'none';
+      document.getElementById('qualitiesTechEnthusiast').style.display = 'none';
+      break;
+
+    case 'tester':
+      document.getElementById('qualitiesTextDev').style.display = 'none';
+      document.getElementById('qualitiesTextMusician').style.display = 'none';
+      document.getElementById('qualitiesTextPhotographer').style.display = 'none';
+      document.getElementById('qualitiesTextTester').style.display = 'block';
+      document.getElementById('qualitiesGraphicDesigner').style.display = 'none';
+      document.getElementById('qualitiesTechEnthusiast').style.display = 'none';
+      break;
+
+    case 'designer':
+      document.getElementById('qualitiesTextDev').style.display = 'none';
+      document.getElementById('qualitiesTextMusician').style.display = 'none';
+      document.getElementById('qualitiesTextPhotographer').style.display = 'none';
+      document.getElementById('qualitiesTextTester').style.display = 'none';
+      document.getElementById('qualitiesGraphicDesigner').style.display = 'block';
+      document.getElementById('qualitiesTechEnthusiast').style.display = 'none';
+      break;
+
+    case 'tech':
+      document.getElementById('qualitiesTextDev').style.display = 'none';
+      document.getElementById('qualitiesTextMusician').style.display = 'none';
+      document.getElementById('qualitiesTextPhotographer').style.display = 'none';
+      document.getElementById('qualitiesTextTester').style.display = 'none';
+      document.getElementById('qualitiesGraphicDesigner').style.display = 'none';
+      document.getElementById('qualitiesTechEnthusiast').style.display = 'block';
+      break;
+  }
+}
