b7938ec1e81f6fa1609446694537ff5cf77b0481
Harris Charalambous <h.chara3@gmail.com>
2021-06-30 22:37:08 +1000

    started work on scroll function

--- assets/script.js
+++ assets/script.js
@@ -85,3 +85,28 @@ function toggleBurger() {
         mobileMenu.classList.add('appear')
     }
 }
+
+function scrollFunction(target, duration) {
+    var target = document.getElementById(target);
+    var targetPosition = target.getBoundingClientRect().top + document.getElementById('header').clientHeight;
+    var startingPosition = window.pageYOffset;
+    var distance = targetPosition - startingPosition;
+    var startTime = null;
+
+    function animate(currentTime){
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed,startingPosition,distance,duration);
+        window.scrollTo(0,run);
+        if (timeElapsed < duration) requestAnimationFrame(animate);
+    }
+
+    function ease (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c /2 * t + b;
+        t--;
+        return -c / 2 * t (t * (t-2) - 1) + b;
+    }
+
+    requestAnimationFrame(animate);
+}
